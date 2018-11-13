756c13432fa9bc68f9268017ad64bcffe1e4c401
jianghaotian <jiang.haotian@trs.com.cn>
2018-11-13 10:00:55 +0800

    测试

---
+++ src/main/java/test/A.java
@@ -0,0 +1,24 @@
+package test;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
+/**
+ * Title:
+ * Description:
+ * Copyright: 2018 北京拓尔思信息技术股份有限公司 版权所有.保留所有权
+ * Company:北京拓尔思信息技术股份有限公司(TRS)
+ * Project: test
+ * Author: jianghaotian
+ * Create Time:2018/11/2 14:48
+ */
+public class A {
+
+    public static void main(String[] args) {
+        System.out.println(System.nanoTime());
+    }
+}
